0716e7b5a25453d42fc3fad56925f97d27454048
zhangxitao <zhangxitao@pku.edu.cn>
2015-11-26 10:44:29 +0800

    HelloWorld

---
+++ HelloWorld/src/edu/pku/ss/zhangxitao/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.pku.ss.zhangxitao;
+
+/**
+ * @author zhangxit
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
